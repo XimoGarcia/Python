0593f03cfca9aec90d7b4d1d6f0fd62b13fecc97
XimoGarcia <jgsastre+github@gmail.com>
2015-01-23 13:26:02 +0100

    Added send to testBox

--- deltas.py
+++ deltas.py
@@ -1,13 +1,16 @@
 import win32clipboard
 import pandas as pd
+import win32com.client
 
 def getTable():
-    table = pd.read_clipboard(header = None, sep=r"[\[\]\t,]")
+    table = pd.read_clipboard(header = None, sep=r"[\[\]\t,\(\)]")
     max1 = table.ix[:, 1].max() + 1
     max2 = table.ix[:, 2].max() + 1
     max3 = table.ix[:, 3].max() + 1
 
     table = table.ix[:, 5].reshape((max1, max2, max3))
+    
+    return table
     
 def quantsTransform(deltas):
     result = zeros((max3, max1, max2))
@@ -25,3 +28,13 @@ def getClipBoard():
     win32clipboard.CloseClipboard()
     
     return data
+
+
+def sendToTextBox(firstWait, innerWait):
+    time.sleep(firstWait)
+    shell = win32com.client.Dispatch("WScript.Shell")    
+    rawTest = getClipBoard()
+    for item in rawTest.splitlines():
+        shell.SendKeys(item) 
+        shell.SendKeys("{TAB}")
+        time.sleep(innerWait/1000.0)
